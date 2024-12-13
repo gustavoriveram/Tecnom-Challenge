9ab728a71409047f7c7c19b222f07e2427467e02
gustavoriveram <guty_rivera97@hotmail.com>
2024-12-12 23:14:57 -0300

    feat: add ngx-toastr library to emulate data sending

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { httpInterceptor } from './shared/interceptors/http.interceptor';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
+    provideToastr(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([httpInterceptor])),
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -1,32 +1,38 @@
 import { Component, inject, signal } from '@angular/core';
-import { ExperienceFormComponent } from "./components/experience-form/experience-form.component";
+import { ExperienceFormComponent } from './components/experience-form/experience-form.component';
 import { Router } from '@angular/router';
 import { WorkshopSurveyService } from './services/workshop-survey.service';
 import { TitleCasePipe } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   imports: [ExperienceFormComponent, TitleCasePipe],
   templateUrl: './survey.component.html',
-  styleUrl: './survey.component.scss'
+  styleUrl: './survey.component.scss',
 })
 export default class SurveyComponent {
-
   router = inject(Router);
   workshopSurveyService = inject(WorkshopSurveyService);
+  toastService = inject(ToastrService);
 
   showMessageView = signal(false);
-  
+
   workshopName = this.workshopSurveyService.workshopName;
   workshopAddress = this.workshopSurveyService.workshopAddress;
   workshopEmail = this.workshopSurveyService.workshopEmail;
   workshopPhone = this.workshopSurveyService.workshopPhone;
 
-  handleFormSubmit(){
+  handleFormSubmit() {
     this.showMessageView.set(true);
+    this.toastService.success('Comentarios enviados!', '', {
+      timeOut: 6000,
+      progressBar: true,
+      progressAnimation: 'increasing',
+    });
     setTimeout(() => {
       // Redirects to home page or some other page
       this.router.navigate(['/']);
       this.showMessageView.set(false);
-    }, 3000);
+    }, 6000);
   }
 }
